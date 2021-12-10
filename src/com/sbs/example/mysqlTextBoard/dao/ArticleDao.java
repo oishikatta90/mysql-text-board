9a138d29d345fee933d3d0fa4ad8185845532733
oishikatta90 <xxxwon90@gmail.com>
2021-12-10 11:25:42 +0900

    이번엔 MySQLUtil로 삭제까지 수정!

--- src/com/sbs/example/mysqlTextBoard/dao/ArticleDao.java
+++ src/com/sbs/example/mysqlTextBoard/dao/ArticleDao.java
@@ -18,14 +18,14 @@ public class ArticleDao {
 
 	public List<Article> getArticles() {
 		List<Article> articles = new ArrayList<Article>();
-		
+
 		SecSql sql = new SecSql();
 		sql.append("SELECT *");
 		sql.append("FROM article");
 		sql.append("ORDER BY id DESC");
-		
+
 		List<Map<String, Object>> articleMapList = MysqlUtil.selectRows(sql);
-		
+
 		for (Map<String, Object> articleMap : articleMapList) {
 			articles.add(new Article(articleMap));
 		}
@@ -37,67 +37,30 @@ public class ArticleDao {
 		sql.append("SELECT *");
 		sql.append("FROM article");
 		sql.append("WHERE id = ?", inputedId);
-		
+
 		Map<String, Object> articleMap = MysqlUtil.selectRow(sql);
-		
+
 		try {
-		if (articleMap.isEmpty()) {
-			return null;
-		}
-		}catch(Exception e) {
+			if (articleMap.isEmpty()) {
+				return null;
+			}
+		} catch (Exception e) {
 			System.out.println("해당 게시물 번호는 없습니다.\n 프로그램을 종료합니다.");
 			System.exit(0);
-		
+
 		}
-		
+
 		return new Article(articleMap);
 	}
 
 	public int delete(int inputedId) {
-		int affectedRows = 0;
-		// 연결 생성
-		Connection conn = null;
-		try {
-			String dbmsJdbcUrl = "jdbc:mysql://127.0.0.1:3306/textBoard?useUnicode=true&characterEncoding=utf-8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull&connectTimeout=60000";
-			String dbmsLoginId = "sbsst";
-			String dbmsLoginPw = "1234";
+		SecSql sql = new SecSql();
+		sql.append("DELETE");
+		sql.append("FROM article");
+		sql.append("WHERE id = ?", inputedId);
 
-			// 기사 등록
-			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
-			} catch (ClassNotFoundException e1) {
-				e1.printStackTrace();
-			}
+		return MysqlUtil.delete(sql);
 
-			try {
-				conn = DriverManager.getConnection(dbmsJdbcUrl, dbmsLoginId, dbmsLoginPw);
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
-//			String sql = "UPDATE article";
-//			sql += " SET updateDate = NOW()";
-//			sql += " WHERE id = 3";
-
-			String sql = "DELETE FROM article WHERE id = ?";
-
-			try {
-				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setInt(1, inputedId);
-				affectedRows = pstmt.executeUpdate();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		} finally {
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return affectedRows;
 	}
 
 	public int write(String title, String body, int memberId, int boardId) {
@@ -141,7 +104,7 @@ public class ArticleDao {
 				pstmt.setInt(3, memberId);
 				pstmt.setInt(4, boardId);
 				pstmt.executeUpdate();
-				
+
 				ResultSet rs = pstmt.getGeneratedKeys();
 				rs.next();
 				id = rs.getInt(1);
