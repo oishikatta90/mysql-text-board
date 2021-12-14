6297973acd73c6945993295f76a6cb0c01a724c8
oishikatta90 <xxxwon90@gmail.com>
2021-12-14 16:04:00 +0900

    만들어 놓은 애들 웹페이지로 띄우기 헤더랑 푸터 적용!

--- src/com/sbs/example/mysqlTextBoard/service/BuildService.java
+++ src/com/sbs/example/mysqlTextBoard/service/BuildService.java
@@ -17,38 +17,35 @@ public class BuildService {
 	}
 
 	public void buildSite() {
+		System.out.println("기존 폴더 삭제");
+		Util.rmdir("site");
 		System.out.println("site/article 폴더 생성");
-		Util.mkdirs("site/article");
+		Util.mkdirs("site");
 		
 		List<Article> articles = articleService.getArticles();
+		
+		String head = Util.getFileContents("site_template/head.html");
+		String foot = Util.getFileContents("site_template/foot.html");
 		
 		for (Article article : articles) {
 			StringBuilder sb = new StringBuilder();
 			
-			sb.append("<!DOCTYPE html>");
-			sb.append("<html lang=\"ko\">");
-			sb.append("<head>");
-			sb.append("<meta charset=\"UTF-8\">");
-			sb.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-			sb.append("<title>게시물 상세페이지 - "+article.title + "</title>");
-			sb.append("</head>");
-			sb.append("<body>");
-			sb.append("<h1>게시물 상세 페이지</h1>");
-			sb.append("<div>");
+			sb.append(head);
+
 			sb.append("번호 : " + article.id + "<br>");
 			sb.append("작성날짜 : " + article.regDate + "<br>");
 			sb.append("갱신날짜 : " + article.updateDate + "<br>");
 			sb.append("제목 : " + article.title + "<br>");
 			sb.append("내용 : " + article.body+ "<br>");
-			sb.append("<a href=\"" + (article.id -1)+ ".html\">이전 글</a><br>");
-			sb.append("<a href=\"" + (article.id +1)+ ".html\">다음 글</a><br>");
+			sb.append("<a href=\"article_detail_" + (article.id -1)+ ".html\">이전 글</a><br>");
+			sb.append("<a href=\"article_detail_" + (article.id +1)+ ".html\">다음 글</a><br>");
 			sb.append("</div>");
-			sb.append("</body>");
-			sb.append("</html>");
 			
-			String fileName	= article.id + ".html"; 
+			sb.append(foot);
+			
+			String fileName	= "article_detail_" + article.id + ".html"; 
 			String filePath = "site/article/";
-			Util.writeFile("site/article/" + fileName, sb.toString());
+			Util.writeFile("site/" + fileName, sb.toString());
 			System.out.println(filePath + "생성");
 			
 			
